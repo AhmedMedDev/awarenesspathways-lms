8137a5af648c79f135920b6cc0fe3019ec2b2c03
unknown <dedoa82.as@gmail.com>
2022-05-29 14:00:25 +0200

    Final Architecture of Personal assensment service has been completed

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -20,7 +20,7 @@
                         </div>
                         <h5 class="info-heading">Personal Assesment</h5>
                         <p class="info-text">Lorem ipsum dolor sit amet, labore et dolore magna aliqua.</p>
-                        <a class="info-link" href="">Discover <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-right"><line x1="5" y1="12" x2="19" y2="12"></line><polyline points="12 5 19 12 12 19"></polyline></svg></a>
+                        <a class="info-link" href="/personal-assesment">Discover <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-right"><line x1="5" y1="12" x2="19" y2="12"></line><polyline points="12 5 19 12 12 19"></polyline></svg></a>
                     </div>
                 </div>
 
